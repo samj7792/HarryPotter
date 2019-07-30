c70f50a16101b0ac115eb30a54fafcff1fc8d466
Cara Sunberg <cnsunberg@gmail.com>
2019-07-30 13:09:32 -0400

    Set up database and API basics

---
+++ api.js
@@ -0,0 +1,11 @@
+var axios = require("axios");
+
+// var ingredient = $("#keyword").val().trim();
+var queryURL = "https://api.edamam.com/search?q=" + "chicken" + "&app_id=" + process.env.EDAMAM_ID + "&app_key=" + process.env.EDAMAM_KEY;
+
+// Perfoming an AJAX GET request to our queryURL
+axios.get(queryURL)
+.then(function(response) {
+    // console.log(response);
+    console.log(JSON.stringify(response.data.hits[0], null, 2));
+});
