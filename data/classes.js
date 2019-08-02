389a70e6b03cc7473264906138a82b5816fde971
Sam Jackson <samuelsjackson7@gmail.com>
2019-08-02 15:44:12 -0400

    added quiz 3 and classes.js for results

---
+++ data/classes.js
@@ -0,0 +1,35 @@
+var classesArr = [
+    {
+        name: "Divination",
+        photo: "",
+        options: ["1","3","1","2","1","4","4","1","3","2"]
+    },{
+        name: "Charms",
+        photo: "",
+        options: ["3","1","2","1","2","1","3","4","2","3"]
+    },{
+        name: "Defense Against the Dark Arts",
+        photo: "",
+        options: ["4","2","2","4","2","1","1","3","3","4"]
+    },{
+        name: "Flying",
+        photo: "",
+        options: ["1","2","4","1","3","3","3","1","4","3"]
+    },{
+        name: "Herbology",
+        photo: "",
+        options: ["3","3","1","3","3","2","2","4","1","2"]
+    },{
+        name: "History of Magic",
+        photo: "",
+        options: ["2","1","4","2","4","3","4","2","1","1"]
+    },{
+        name: "Potions",
+        photo: "",
+        options: ["4","4","3","3","4","2","2","3","4","1"]
+    },{
+        name: "Transfiguration",
+        photo: "",
+        options: ["2","4","3","4","1","4","1","2","2","4"]
+    },
+]
