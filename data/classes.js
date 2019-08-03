7a9b489b235a93c96e4c7ff2a439f8782baefcc9
Sam Jackson <samuelsjackson7@gmail.com>
2019-08-03 14:27:33 -0400

    added photos to classes array

--- data/classes.js
+++ data/classes.js
@@ -1,42 +1,42 @@
 var classesArr = [
   {
     name: "Divination",
-    photo: "",
+    photo: "https://vignette.wikia.nocookie.net/harrypotter/images/4/40/DivinationHarryRon.jpg/revision/latest/scale-to-width-down/2000?cb=20161207020758",
     options: ["1", "3", "1", "2", "1", "4", "4", "1", "3", "2"]
   },
   {
     name: "Charms",
-    photo: "",
+    photo: "https://amp.businessinsider.com/images/56956e08dd08959a768b4785-750-308.png",
     options: ["3", "1", "2", "1", "2", "1", "3", "4", "2", "3"]
   },
   {
     name: "Defense Against the Dark Arts",
-    photo: "",
+    photo: "https://miro.medium.com/max/1838/1*y-aPR4CC8PD_s8638FkVnA.jpeg",
     options: ["4", "2", "2", "4", "2", "1", "1", "3", "3", "4"]
   },
   {
     name: "Flying",
-    photo: "",
+    photo: "https://i.pinimg.com/originals/b4/e2/a3/b4e2a34bc39623ce9e44b64d1f7575b1.jpg",
     options: ["1", "2", "4", "1", "3", "3", "3", "1", "4", "3"]
   },
   {
     name: "Herbology",
-    photo: "",
+    photo: "https://harry-potter-sounds.ambient-mixer.com/images_template/3/9/9/399b136138d984ecd8a7c541054e7325_full.jpg",
     options: ["3", "3", "1", "3", "3", "2", "2", "4", "1", "2"]
   },
   {
     name: "History of Magic",
-    photo: "",
+    photo: "https://harry-potter-sounds.ambient-mixer.com/images_template/f/8/c/f8c9424e246e899e2a77a82476239e5f_full.jpg",
     options: ["2", "1", "4", "2", "4", "3", "4", "2", "1", "1"]
   },
   {
     name: "Potions",
-    photo: "",
+    photo: "https://vignette.wikia.nocookie.net/harrypotter/images/a/a0/Sorcerers-stone_snape.jpg/revision/latest?cb=20180523021403",
     options: ["4", "4", "3", "3", "4", "2", "2", "3", "4", "1"]
   },
   {
     name: "Transfiguration",
-    photo: "",
+    photo: "https://www.filminquiry.com/wp-content/uploads/2019/01/McGonagall_in_classroom-PS-750x500.jpg",
     options: ["2", "4", "3", "4", "1", "4", "1", "2", "2", "4"]
   }
 ];
