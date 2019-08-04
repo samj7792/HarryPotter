38e227360704afd5fdd57e542219260454ae9bff
Cara Sunberg <cnsunberg@gmail.com>
2019-08-04 18:48:26 -0400

    Cleaned up commented out code, added comments to stylesheets, removed unused files

--- public/js/audio.js
+++
@@ -1,9 +0,0 @@
-console.log("READY");
-$(document).ready(function() {
-//   console.log("ready!");
-//   $("audio#fireworks")[0].pause();
-//   setTimeout(audio, 500);
-//   function audio() {
-//     $("audio#fireworks")[0].play();
-//   }
-});
