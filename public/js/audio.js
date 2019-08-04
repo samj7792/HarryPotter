4cfed0af32ec9b97f5038b91de242cd2d878fffa
Cara Sunberg <cnsunberg@gmail.com>
2019-08-03 22:38:16 -0400

    Made title font larger for quizzes, added audio to results but sometimes doesn't autoplay because of DOM exception

---
+++ public/js/audio.js
@@ -0,0 +1,9 @@
+console.log("READY");
+$(document).ready(function() {
+//   console.log("ready!");
+//   $("audio#fireworks")[0].pause();
+//   setTimeout(audio, 500);
+//   function audio() {
+//     $("audio#fireworks")[0].play();
+//   }
+});
