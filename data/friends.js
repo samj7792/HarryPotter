0324fcde6aca8f11de47390369816c4fc8cefd4f
Cara Sunberg <cnsunberg@gmail.com>
2019-08-03 20:52:42 -0400

    Made minor edits to questions and answers in quizzes 2 and 3

--- data/friends.js
+++ data/friends.js
@@ -3,31 +3,31 @@ var friendsArr = [
     name: "Ronald Weasley",
     photo:
       "https://images.pottermore.com/bxd3o8b291gf/4kRGmTOvVYmIwsOikwcuUw/8b8f2e4ffcc1a5218bafe615f976abb0/Ron_Weasley_WB_F1_Ron_with_Scabbers_on_shoulder_00393692.jpg?w=1200",
-    options: ["1", "2", "4", "4", "3", "1", "2", "4", "4", "3"]
+    options: ["3", "3", "4", "3", "2", "3", "4", "4", "2", "3"]
   },
   {
     name: "Padma Patil",
     photo:
       "https://vignette.wikia.nocookie.net/harrypotter/images/4/45/PadmaPatil.png/revision/latest?cb=20170118054329",
-    options: ["2", "3", "3", "3", "4", "2", "4", "3", "3", "4"]
+    options: ["2", "3", "2", "4", "4", "1", "1", "1", "4", "4"]
   },
   {
     name: "Cedric Diggory",
     photo:
       "https://images.pottermore.com/bxd3o8b291gf/1uZurZSTWwcC6Ww8UcQ8ws/e3ce3b91e3aa2a86ce8aaed542d62f98/CedricDiggory_WB_F4_CedricDiggoryWithHandsInPockets_Promo_080615_Port.jpg?w=1200",
-    options: ["3", "4", "3", "2", "1", "4", "3", "3", "1", "1"]
+    options: ["1", "2", "2", "1", "1", "2", "2", "2", "3", "1"]
   },
   {
     name: "Vincent Crabbe",
     photo:
       "https://vignette.wikia.nocookie.net/harrypotter/images/b/ba/Vincent_Crabbe.jpg/revision/latest/scale-to-width-down/2000?cb=20091224183746",
-    options: ["1", "1", "1", "2", "1", "1", "1", "2", "2", "2"]
+    options: ["4", "1", "1", "2", "1", "4", "4", "4", "2", "4"]
   },
   {
     name: "Cho Chang",
     photo:
       "https://images.pottermore.com/bxd3o8b291gf/4PbTuJ9S2QoEwycW4gs4Mg/21f94bfe4081438a9bc3c159c2831b5a/ChoChang_WB_F5_ChoChangPointingWand_Promo_080615_Port.jpg?w=1200",
-    options: ["4", "2", "2", "1", "3", "3", "3", "1", "1", "4"]
+    options: ["1", "4", "3", "3", "3", "3", "3", "3", "1", "2"]
   }
 ];
 
